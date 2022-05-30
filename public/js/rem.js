1b80288116bfaab1a92a091cdc733f913fce2542
wzk <1356422517@qq.com>
2022-05-31 00:22:39 +0800

    rem适配

--- public/js/rem.js
+++ public/js/rem.js
@@ -1,13 +1,13 @@
-function remSize(){
+function remSize () {
   let win = document.documentElement.clientWidth
   //win/7.5表示将任意大小窗口分成100份 则：100rem === 窗口宽度
-  document.documentElement.style.fontSize = win/7.5 + "px"
+  document.documentElement.style.fontSize = win / 7.5 + "px"
   //设置默认的字体大小
-  document.querySelector("body").style.fontSize = 0.3+"rem"
+  document.querySelector("body").style.fontSize = 0.3 + "rem"
 
 }
 
 remSize()
-window.onresize = function() {
+window.onresize = function () {
   remSize()
 }
