f666bce43727e0e0ec4384b5a6b2bb6755e9683e
wzk <1356422517@qq.com>
2023-02-15 17:14:54 +0800

    更改apiURL

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-// const baseURL = process.env.NODE_ENV === 'development' ? "http://localhost:3000" : ""
-const baseURL = "https://music-api-ten-sand.vercel.app"
+const baseURL = "https://www.codeman.ink/api"
 
 axios.defaults.baseURL = baseURL
 
